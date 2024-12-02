89018e7a0ae0eb38fa9ec252b925182cf6ddf14c
lmagitem <lucmagitem@gmail.com>
2024-12-02 21:08:10 +0100

    Fixed non-functional code, next step will be to fix the logic/tests

--- src/galaxy/map/division/generator.rs
+++ src/galaxy/map/division/generator.rs
@@ -1,7 +1,9 @@
 use crate::internal::*;
 use crate::prelude::*;
 use std::collections::HashMap;
-use usvg::{Tree, Options, NodeKind, OptionsRef, PathSegment, Paint};
+use usvg::tiny_skia_path::PathSegment;
+use usvg::Node::Path;
+use usvg::{Options, Paint, Tree};
 
 impl GalacticMapDivision {
     pub fn generate(
@@ -82,33 +84,54 @@ fn generate_region(coord: SpaceCoordinates, galaxy: &Galaxy) -> GalacticRegion {
     let layer_index = if layer_count == 1 {
         0
     } else {
-        ((z as f64 / galaxy.get_galaxy_size().z as f64) * (layer_count as f64 - 1.0)).round() as usize
+        ((z as f64 / galaxy.get_galaxy_size().z as f64) * (layer_count as f64 - 1.0)).round()
+            as usize
     };
 
     let svg_data = &galaxy.galactic_map_layers[layer_index];
-    let tree = Tree::from_str(svg_data, &OptionsRef::default()).unwrap();
+    let opt = Options::default();
+    let tree = Tree::from_str(svg_data, &opt).unwrap();
 
     let mut color_map = HashMap::new();
-    for node in tree.root().descendants() {
-        if let NodeKind::Path(ref path) = *node.borrow() {
-            if let Some(ref fill) = path.fill {
-                if let Paint::Color(color) = fill.paint {
-                    color_map.insert(path.data.to_vec(), format!("{:06X}", color));
+    for node in tree.root().children() {
+        if let Path(ref path) = *node {
+            if let Some(ref fill) = path.fill() {
+                if let Paint::Color(color) = fill.paint() {
+                    let usvg::Color { red, green, blue } = color;
+                    let color_string = format!("{:02X}{:02X}{:02X}", red, green, blue);
+
+                    // Convert path data to a serializable format
+                    let path_segments: Vec<(i32, i32)> = path
+                        .data()
+                        .segments()
+                        .filter_map(|segment| match segment {
+                            PathSegment::MoveTo(point) | PathSegment::LineTo(point) => {
+                                Some((point.x.round() as i32, point.y.round() as i32))
+                            }
+                            _ => None,
+                        })
+                        .collect();
+
+                    // Convert path segments to a key that implements `Eq` and `Hash`
+                    let key = format!("{:?}", path_segments);
+                    color_map.insert(key, color_string);
                 }
             }
         }
     }
 
-    let x = coord.x;
-    let y = coord.y;
+    let x = coord.x as f64;
+    let y = coord.y as f64;
     let mut closest_color = "000000".to_string();
     let mut min_distance = f64::MAX;
 
-    for (path_data, color) in color_map {
-        for segment in path_data.windows(2) {
-            let (x1, y1) = (segment[0].x, segment[0].y);
-            let (x2, y2) = (segment[1].x, segment[1].y);
-            let distance = ((x2 - x1).powi(2) + (y2 - y1).powi(2)).sqrt();
+    for (path_segments_key, color) in color_map {
+        // Deserialize the `path_segments_key` back into a vector of `(i32, i32)`
+        let path_segments: Vec<(i32, i32)> =
+            serde_json::from_str(&path_segments_key).unwrap_or_else(|_| Vec::new());
+
+        for (x1, y1) in path_segments {
+            let distance = ((x - x1 as f64).powi(2) + (y - y1 as f64).powi(2)).sqrt();
             if distance < min_distance {
                 min_distance = distance;
                 closest_color = color.clone();
@@ -116,7 +139,7 @@ fn generate_region(coord: SpaceCoordinates, galaxy: &Galaxy) -> GalacticRegion {
         }
     }
 
-    match closest_color.as_str() {
+    let region = match closest_color.as_str() {
         "000000" => GalacticRegion::Void,
         "111111" => GalacticRegion::Aura,
         "222222" => GalacticRegion::Exile,
@@ -134,7 +157,9 @@ fn generate_region(coord: SpaceCoordinates, galaxy: &Galaxy) -> GalacticRegion {
         "EEEEEE" => GalacticRegion::Core,
         "FFFFFF" => GalacticRegion::Nucleus,
         _ => GalacticRegion::Multiple,
-    }
+    };
+
+    region
 }
 
 #[cfg(test)]
@@ -145,11 +170,12 @@ mod tests {
     #[test]
     fn test_generate_region_with_default_svg() {
         let galaxy = Galaxy {
-            galactic_map_layers: vec![r#"
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
-                    <rect x="0" y="0" width="100" height="100" fill="#000000" />
-                </svg>
-            "#.to_string()],
+            galactic_map_layers: vec![r##"
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+        <rect x="0" y="0" width="100" height="100" fill="#000000" />
+    </svg>
+"##
+            .to_string()],
             ..Default::default()
         };
 
@@ -162,16 +188,18 @@ mod tests {
     fn test_generate_region_with_multiple_svg_layers() {
         let galaxy = Galaxy {
             galactic_map_layers: vec![
-                r#"
+                r##"
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
                     <rect x="0" y="0" width="100" height="100" fill="#000000" />
                 </svg>
-                "#.to_string(),
-                r#"
+                "##
+                .to_string(),
+                r##"
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
                     <rect x="0" y="0" width="100" height="100" fill="#FFFFFF" />
                 </svg>
-                "#.to_string(),
+                "##
+                .to_string(),
             ],
             ..Default::default()
         };
@@ -189,16 +217,18 @@ mod tests {
     fn test_generate_region_with_interpolation() {
         let galaxy = Galaxy {
             galactic_map_layers: vec![
-                r#"
+                r##"
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
                     <rect x="0" y="0" width="100" height="100" fill="#000000" />
                 </svg>
-                "#.to_string(),
-                r#"
+                "##
+                .to_string(),
+                r##"
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
                     <rect x="0" y="0" width="100" height="100" fill="#FFFFFF" />
                 </svg>
-                "#.to_string(),
+                "##
+                .to_string(),
             ],
             ..Default::default()
         };
